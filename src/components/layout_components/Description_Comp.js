55f8071e51c43b10d9a785e890b66a518a872329
tboy44wiz <tosolife@yahoo.com>
2021-08-04 10:54:06 +0100

    Updated the Landing Page with the Description Component.

--- src/components/layout_components/Description_Comp.js
+++ src/components/layout_components/Description_Comp.js
@@ -10,14 +10,16 @@ const DescriptionComp = () => {
     return (
         <div className="AboutDescriptionComp">
 
-            <SectionTitleComp hint="We offer you our belgium cars and spare parts." title="Welcome to Jogabest Global Investment" />
+            <SectionTitleComp hint="Welcome to Jogabest Global Investment." title="Who we are" />
 
             <div className="container">
                 <div className="row">
                     <div className="col">
                         <p className="description__wrapper">
-                            <span>Jogabest Global Investment Ltd</span> is
-                            one of the fastest growing and diversified business
+                            <span>Jogabest Global Investment Ltd,</span> Importer and Exporter of all kinds of Vehicles,
+                            Automobiles Spare Parts, Motorcyles, Generators, Electronics, Boat Engines, Home Appliances,
+                            Textile, Agro-products, Etc. <br />
+                            We are one of the fastest growing and diversified business
                             conglomerates incorporated in Nigeria with a hard earned reputation for excellent business
                             practices and products superiority. The major source of our goods is Japan and that attests
                             to the quality and standardization of our product lines.
--- src/components/pages_components/LandingPage_Comp.js
+++ src/components/pages_components/LandingPage_Comp.js
@@ -7,12 +7,18 @@ import TopBrandsComp from "../layout_components/TopBrands_Comp";
 import MostRecentProductsComp from "../layout_components/MostRecentProducts_Comp";
 import VehicleListingComp from "../layout_components/VehicleListing_Comp";
 import HappyCustomersComp from "../layout_components/HappyCustomers_Comp";
+import DescriptionComp from "../layout_components/Description_Comp";
+import CoreValuesComp from "../layout_components/CoreValues_Comp";
 
 
 const LandingPageComp = () => {
     return (
         <AppLayoutHOC>
             <AppCarouselComp />
+
+            <DescriptionComp />
+
+            <CoreValuesComp />
 
             <TopBrandsComp />
 
