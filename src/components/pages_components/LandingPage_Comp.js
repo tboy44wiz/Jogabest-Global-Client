972dd0692c5ce35cdefedfe23df40b98e17d4573
tboy44wiz <tosolife@yahoo.com>
2021-08-26 12:16:32 +0100

    Updateing the AboutUs Components Again.

--- src/components/pages_components/LandingPage_Comp.js
+++ src/components/pages_components/LandingPage_Comp.js
@@ -6,9 +6,9 @@ import AppCarouselComp from "../layout_components/AppCarousel_Comp";
 import TopBrandsComp from "../layout_components/TopBrands_Comp";
 import MostRecentProductsComp from "../layout_components/MostRecentProducts_Comp";
 import VehicleListingComp from "../layout_components/VehicleListing_Comp";
-import HappyCustomersComp from "../layout_components/HappyCustomers_Comp";
 import DescriptionComp from "../layout_components/Description_Comp";
 import CoreValuesComp from "../layout_components/CoreValues_Comp";
+// import HappyCustomersComp from "../layout_components/HappyCustomers_Comp";
 
 
 const LandingPageComp = () => {
