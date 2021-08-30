1a919f6a6e6d79e70f41d1010161fe36d27c3a88
tboy44wiz <tosolife@yahoo.com>
2021-08-30 20:22:48 +0100

    Changing the Carousel in the home page.

--- src/components/layout_components/AppCarousel_Comp.js
+++ src/components/layout_components/AppCarousel_Comp.js
@@ -7,44 +7,40 @@ import "normalize.css/normalize.css";
 import '../../scss/layout_components/_AppCarousel_Comp.scss';
 
 //  Import Slider Images.
-import Image1 from "../../assets/images/slider_images/image1.jpg";
-import Image2 from "../../assets/images/slider_images/image2.jpg";
-import Image3 from "../../assets/images/slider_images/image3.jpg";
-import Image4 from "../../assets/images/slider_images/image4.jpg";
+import Image1 from "../../assets/images/slider_images/jogabest_mini_buses.png";
+import Image2 from "../../assets/images/slider_images/jogabest_boat_engines.png";
+import Image3 from "../../assets/images/slider_images/jogabest_motor_parts.png";
+import Image4 from "../../assets/images/slider_images/jogabest_car_engines.png";
 
 //  Slider content.
 const slides = [
     {
-        title: "Vulputate Mollis Ultricies Fermentum Parturient",
-        description:
-            "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+        title: "Jogabest Mini Buses",
+        description: "Suzuki every mini bus is a small bus used in Lagos and some other parts in Nigeria. It has low fuel consumption and this bus is easy maintenance.",
         button: "Read More",
         image: Image1,
         user: 'Luan Gjokaj',
         userProfile: 'https://i.imgur.com/JSW6mEk.png'
     },
     {
-        title: "Tortor Dapibus Commodo Aenean Quam",
-        description:
-            "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+        title: "Jogabest Boat Engines",
+        description: "Yamaha motor Boat engines.",
         button: "Discover",
         image: Image2,
         user: "Erich Behrens",
         userProfile: "https://i.imgur.com/0Clfnu7.png"
     },
     {
-        title: "Phasellus volutpat metus",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+        title: "Jogabest Motor Parts",
+        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
         button: "Buy now",
         image: Image3,
         user: "Bruno Vizovskyy",
         userProfile: "https://i.imgur.com/4KeKvtH.png"
     },
     {
-        title: "Phasellus volutpat metus",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+        title: "Jogabest Car Engines",
+        description: "Get your fairly used car engines of different car brands.",
         button: "Buy now",
         image: Image4,
         user: "Bruno Vizovskyy",
@@ -60,14 +56,15 @@ const AppCarouselComp = () => {
                     key={index}
                     className="slider__content"
                     style={{
-                        background: `linear-gradient(90deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
-                        url("${slide.image}") no-repeat center center #43888F`
+                        background: `url("${slide.image}") no-repeat center center #43888F`
+                        /*background: `linear-gradient(90deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+                        url("${slide.image}") no-repeat center center #43888F`*/
                     }}
                 >
                     <div className="inner">
                         <h1>{slide.title}</h1>
                         <p>{slide.description}</p>
-                        <button>{slide.button}</button>
+                        {/*<button>{slide.button}</button>*/}
                     </div>
                 </div>
             ))}
